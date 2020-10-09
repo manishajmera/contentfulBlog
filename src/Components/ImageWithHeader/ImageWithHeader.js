75539b8429823779e2a4da25fc2b1da9b24fd8d3
manishajmera27 <manishajmera27@gmail.com>
2020-10-09 21:34:18 +0530

    some css changes

--- src/Components/ImageWithHeader/ImageWithHeader.js
+++ src/Components/ImageWithHeader/ImageWithHeader.js
@@ -3,7 +3,7 @@ import "./imageWithHeader.css";
 
 export default function ImageWithHeader({title}) {
     return (
-        <div className="row paddingCls">
+        <div className="paddingCls">
             <div className="bkgImg" style={{backgroundImage: `url(${window.location.origin}/default-image.jpg)`}}>
                 <h2 className="caption">{title}</h2>
             </div>
